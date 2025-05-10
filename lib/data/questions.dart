5918c0be6b1a67139a1601369bc591f23856bfcb
AlbertoJTD <albertojtd@outlook.com>
2025-05-10 06:42:04 -0600

    Add questions list

---
+++ lib/data/questions.dart
@@ -0,0 +1,32 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'What are the main building blocks of Flutter UIs?',
+    ['Widgets', 'Components', 'Elements', 'Blocks'],
+  ),
+  QuizQuestion(
+  'Which widgets are used for horizontal and vertical layouts in Flutter?',
+  ['Row & Column', 'Stack & Container', 'ListView & GridView', 'Padding & Align'],
+),
+  QuizQuestion(
+    'What is the purpose of the pubspec.yaml file in a Flutter project?',
+    ['Manage dependencies', 'Define UI layout', 'Store images', 'Configure settings'],
+  ),
+  QuizQuestion(
+    'What is the main function of the MaterialApp widget in Flutter?',
+    ['Set up app theme and routing', 'Create a scrollable list', 'Display images', 'Handle user input'],
+  ),
+  QuizQuestion(
+    'What feature allows instant UI updates during Flutter development?',
+    ['Hot Reload', 'Hot Restart', 'Debug Mode', 'Incremental Compilation'],
+  ),
+  QuizQuestion(
+    'Which widget is used for dynamic, mutable content?',
+    ['StatefulWidget', 'StatelessWidget', 'InheritedWidget', 'ProxyWidget'],
+  ),
+  QuizQuestion(
+    'Which programming language is Flutter primarily built with?',
+    ['Dart', 'Kotlin', 'JavaScript', 'Java'],
+  ),
+];
