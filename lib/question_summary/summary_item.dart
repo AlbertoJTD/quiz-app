64c6bf55437e24540e5eb62d063655c5c3f732fb
AlbertoJTD <albertojtd@outlook.com>
2025-05-10 16:33:37 -0600

    Add padding to SummaryItem

--- lib/question_summary/summary_item.dart
+++ lib/question_summary/summary_item.dart
@@ -10,49 +10,52 @@ class SummaryItem extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        QuestionIdentifier(
-          questionIndex: ((itemData['question_index'] as int) + 1),
-          isCorrectAnswer: itemData['is_correct'] as bool,
-        ),
-
-        const SizedBox(width: 5),
-
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                itemData['question'] as String,
-                style: GoogleFonts.lato(
-                  color: Colors.white,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold
-                )
-              ),
-              const SizedBox(height: 5),
-
-              Text(
-                itemData['user_answer'] as String,
-                style: const TextStyle(
-                  color: Color.fromARGB(255, 202, 171, 252),
-                  fontSize: 16,
-                ),
-              ),
-
-              Text(
-                itemData['correct_answer'] as String,
-                style: const TextStyle(
-                  color: Color.fromARGB(255, 181, 254, 246),
-                  fontSize: 16,
-                ),
-              ),
-            ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          QuestionIdentifier(
+            questionIndex: ((itemData['question_index'] as int) + 1),
+            isCorrectAnswer: itemData['is_correct'] as bool,
           ),
-        ),
-      ],
+      
+          const SizedBox(width: 5),
+      
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  itemData['question'] as String,
+                  style: GoogleFonts.lato(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold
+                  )
+                ),
+                const SizedBox(height: 5),
+      
+                Text(
+                  itemData['user_answer'] as String,
+                  style: const TextStyle(
+                    color: Color.fromARGB(255, 202, 171, 252),
+                    fontSize: 16,
+                  ),
+                ),
+      
+                Text(
+                  itemData['correct_answer'] as String,
+                  style: const TextStyle(
+                    color: Color.fromARGB(255, 181, 254, 246),
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
