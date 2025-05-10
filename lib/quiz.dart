f8afcc3781ca359e34959e5dfdd4fec680a436ac
AlbertoJTD <albertojtd@outlook.com>
2025-05-10 14:03:11 -0600

    Add structure of ResultsScreen widget

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/start_page.dart';
 import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/results_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -35,7 +36,7 @@ class _QuizState extends State<Quiz> {
 
     if (selectedAnswers.length == questions.length) {
       setState(() {
-        activeScreen = StartPage(switchScreen);
+        activeScreen = ResultsScreen();
         selectedAnswers.clear();
       });
     }
---
+++ lib/results_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class ResultsScreen extends StatelessWidget {
+  const ResultsScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return SizedBox(
+      width: double.infinity, // Take the full width of the screen
+      child: Container(
+        margin: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'You answered X out of Y questions correctly!',
+            ),
+
+            SizedBox(height: 20),
+
+            Text(
+              'Here goes the list of answers',
+            ),
+
+            // ListView(
+            //   scrollDirection: Axis.vertical,
+            //   children: [
+            //     Text(
+            //       'Question 1: Correct',
+            //       style: TextStyle(
+            //         color: Colors.green,
+            //         fontSize: 18,
+            //       ),
+            //     ),
+            //     Text(
+            //       'Question 2: Incorrect',
+            //       style: TextStyle(
+            //         color: Colors.red,
+            //         fontSize: 18,
+            //       ),
+            //     ),
+            //     Text(
+            //       'Question 3: Correct',
+            //       style: TextStyle(
+            //         color: Colors.green,
+            //         fontSize: 18,
+            //       ),
+            //     ),
+            //   ]
+            // ),
+
+            SizedBox(height: 20),
+
+            TextButton.icon(
+              onPressed: () {},
+              label: Text(
+                'Restart Quiz'
+              ),
+              icon: Icon(
+                Icons.refresh_outlined
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
