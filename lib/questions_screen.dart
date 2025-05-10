6158375495b7544d8fee421614db87ee88986a91
AlbertoJTD <albertojtd@outlook.com>
2025-05-10 11:38:28 -0600

    Generated dynamic AwnswerButton widgets

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -14,36 +15,29 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity, // Take the full width of the screen
       child:  Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
-            'Here goes a question!',
+            currentQuestion.question,
             style: TextStyle(
               color:Colors.white,
             ),
           ),
-      
+
           SizedBox(height: 20),
-      
-          AwnswerButton(
-            answerText: 'Answer 1',
-            onTap: () {}
-          ),
-          AwnswerButton(
-            answerText: 'Answer 1',
-            onTap: () {}
-          ),
-          AwnswerButton(
-            answerText: 'Answer 1',
-            onTap: () {}
-          ),
-          AwnswerButton(
-            answerText: 'Answer 1',
-            onTap: () {}
-          ),
+
+          // Spreading values
+          ...currentQuestion.answers.map((answer) {
+            return AwnswerButton(
+              answerText: answer,
+              onTap: () {},
+            );
+          }),
         ],
       ),
     );
